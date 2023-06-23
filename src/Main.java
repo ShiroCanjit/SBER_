f86895aa67dc1e55233fc2d945fb62f82abbe1e8
ShiroCanjit <92326618+ShiroCanjit@users.noreply.github.com>
2023-06-23 18:09:04 +0300

    part 3

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int counter = 0;
+        String fileNameDefined = "source/Задача ВС Java Сбер.csv";
+        File file = new File(fileNameDefined);
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileNameDefined))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                counter++;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            Scanner inputStream = new Scanner(file);
+            City[] city = new City[counter];
+            while (inputStream.hasNext()) {
+                String inLine = inputStream.nextLine();
+                for (int i = 0; i < inLine.lines().count(); i++) {
+                    Scanner inWord = new Scanner(inLine);
+                    inWord.useDelimiter(";");
+                    int id = inWord.nextInt();
+                    city[i] = new City(inWord.next(), inWord.next(), inWord.next(),
+                            inWord.next(), (inWord.hasNext()) ? inWord.next() : "");
+                    System.out.println(city[i].ShowObject());
+                    inWord.close();
+                }
+            }
+            inputStream.close();
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
+
+class City {
+    String name, region, district, foundation, population;
+
+    public City(String name, String region, String district, String population, String foundation) {
+        this.name = name;
+        this.region = region;
+        this.district = district;
+        this.population = population;
+        this.foundation = foundation;
+    }
+
+    public String ShowObject() {
+        String outLine = "City{name ='" + this.name +
+                "', region='" + this.region +
+                "', district='" + this.district +
+                "', population='" + this.population +
+                "', foundation='" + this.foundation + "'}";
+        return outLine;
+    }
+
+}
