22168934f55a7b299ad752c46d15a8e98756f4ae
ShiroCanjit <92326618+ShiroCanjit@users.noreply.github.com>
2023-06-26 19:25:08 +0300

    bugfix task_2 V2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
+    static City[] cities;
+
     public static void main(String[] args) {
         int counter = 0;
         String fileNameDefined = "source/Задача ВС Java Сбер.csv";
@@ -14,37 +18,59 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
+        cities = new City[counter];
+        Scanner inputStream;
         try {
-            Scanner inputStream = new Scanner(file);
-            City[] cities = new City[counter];
-            counter=0;
+            inputStream = new Scanner(file);
+            counter = 0;
             while (inputStream.hasNext()) {
                 String inLine = inputStream.nextLine();
-                    Scanner inWord = new Scanner(inLine);
-                    inWord.useDelimiter(";");
-                    inWord.nextInt();
-                    cities[counter] = new City(inWord.next(), inWord.next(), inWord.next(),
-                            inWord.next(), (inWord.hasNext()) ? inWord.next() : "");
-                   // System.out.println(cities[counter].toString());
-                    inWord.close();
+                Scanner inWord = new Scanner(inLine);
+                inWord.useDelimiter(";");
+                inWord.nextInt();
+                cities[counter] = new City(inWord.next(), inWord.next(), inWord.next(),
+                        inWord.next(), (inWord.hasNext()) ? inWord.next() : "");
+                System.out.println(cities[counter].toString());
+                inWord.close();
                 counter++;
-                }
+            }
             inputStream.close();
 
-
-            System.out.println(Arrays.toString(cities));
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-
+        inputStream = new Scanner(System.in);
+        System.out.println("Введите 1, для сортировки по наименованию, и 2  для сортировки по двум полям справочника " +
+                "– федеральному округу и наименованию города");
+        int chooser = inputStream.nextInt();
+        switch (chooser) {
+            case 1 -> nameCompare();
+            case 2 -> districtNameCompare();
+            default -> System.out.println("Ошибка Вода");
+        }
+        System.out.println(Arrays.toString(cities));
 
     }
+
+    static void districtNameCompare() {
+        nameCompare();
+        Arrays.sort(cities, (first, second) -> {
+            if (!Objects.equals(first.getDistrict(), second.getDistrict())) {
+                return first.getDistrict().compareTo(second.getDistrict());
+            }
+            return first.getName().compareTo(second.getName());
+        });
+    }
+
+    static void nameCompare() {
+        Arrays.sort(cities, Comparator.comparing(City::getName));
+    }
+
 }
 
-class City implements Comparable<City>{
-     String name, region, district, foundation, population;
+class City implements Comparable<City> {
+    String name, region, district, foundation, population;
 
     public City(String name, String region, String district, String population, String foundation) {
         this.name = name;
@@ -53,24 +79,28 @@ class City implements Comparable<City>{
         this.population = population;
         this.foundation = foundation;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public String getDistrict(){
+
+    public String getDistrict() {
         return district;
     }
+
     @Override
     public int compareTo(City o) {
         return this.name.compareTo(o.getName());
     }
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "City{name ='" + this.name +
                 "', region='" + this.region +
                 "', district='" + this.district +
                 "', population='" + this.population +
                 "', foundation='" + this.foundation + "'}";
     }
+
 
 }
